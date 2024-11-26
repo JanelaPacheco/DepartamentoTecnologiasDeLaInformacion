94b8926ab6a4638d30769b01cd80488f617b4b6c
janela <pachecojanela06@gmail.com>
2024-11-26 11:33:09 -0500

    Tecnologias de la Informacion

---
+++ src/main/java/com/mycompany/departametotecnologiasdelainformacion/DepartametoTecnologiasDeLaInformacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.departametotecnologiasdelainformacion;
+
+/**
+ *
+ * @author pache
+ */
+public class DepartametoTecnologiasDeLaInformacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
