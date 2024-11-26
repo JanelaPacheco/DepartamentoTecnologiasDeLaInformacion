391ed17cfa7565ce51edc4970fcc6b798e02c2dc
janela <pachecojanela06@gmail.com>
2024-11-26 11:37:55 -0500

    Proyecto Tecnologia

--- src/main/java/com/mycompany/departametotecnologiasdelainformacion/DepartametoTecnologiasDeLaInformacion.java
+++ src/main/java/com/mycompany/departametotecnologiasdelainformacion/DepartametoTecnologiasDeLaInformacion.java
@@ -12,5 +12,6 @@ public class DepartametoTecnologiasDeLaInformacion {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("BIENVENIDO AL DEPARTAMENTO DE TECNOLOGIAS DE LA INFORMACION");
     }
 }
